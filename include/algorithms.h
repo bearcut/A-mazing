ed1d494f149ce896092b92ddb45191a80a64c9a7
bearcut <161555447+bearcut@users.noreply.github.com>
2026-04-20 14:45:07 +0530

    Refactor convertToBraidMaze function signature
    Updated the function signature for convertToBraidMaze to clarify the parameter name and added a comment about its purpose.

--- include/algorithms.h
+++ include/algorithms.h
@@ -13,9 +13,9 @@ void* solveAstar(void* arg);
 void* solveDijkstra(void* arg);
 void* solveBFS(void* arg);
 void* solveDFS(void* arg);
-void convertToBraidMaze(maze* m, double extraWallRemovalChance);
+void convertToBraidMaze(maze* m, double RemovalChance);//braid maze is for adding additional solutions for the algos to show varied results
 
-// Benchmark Tool
+// Benchmark Tool for bechmarking
 void runBenchmark(maze* m);
 
 #endif 
